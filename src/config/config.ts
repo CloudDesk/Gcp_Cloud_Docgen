164d3b50d441385da6ccb262b627e4f222f5b739
pravin-on-cloud <pravinraja@clouddesk.ae>
2024-11-13 13:03:29 +0530

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -8,10 +8,6 @@ export const SF_ORG_ID = process.env.SF_ORG_ID;
 export const SF_USERNAME = process.env.SF_USERNAME;
 export const SF_CLIENT_ID = process.env.SF_CLIENT_ID;
 
-if (!port) {
-  console.error("Error: PORT environment variable is not defined.");
-  process.exit(1);
-}
 
 export const PORT = Number(port);
 
