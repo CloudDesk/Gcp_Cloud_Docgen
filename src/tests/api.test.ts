17b6168f7643ff7963e71cd8e805d93a7a09e1e6
pravin-on-cloud <pravinraja@clouddesk.ae>
2024-11-18 18:07:19 +0530

    Update api.test.ts

--- src/tests/api.test.ts
+++ src/tests/api.test.ts
@@ -7,160 +7,160 @@ const SF_ORG_ID = process.env.SF_ORG_ID;
 
 
 
-// describe('POST Initial test', () => {
-//   let fastify: any;
+describe('POST Initial test', () => {
+  let fastify: any;
 
-//   beforeAll(async () => {
-//     fastify = await initializeApp(); // Initialize the app instance
-//   });
+  beforeAll(async () => {
+    fastify = await initializeApp(); // Initialize the app instance
+  });
 
-//   it('should return  200 ', async () => {
-//     const response = await fastify.inject({
-//       method: 'get',
-//       url: '/',
-//       headers: {
-//         'X-API-KEY': API_KEY,
-//       },
-//     });
-//     console.log(response, 'Response for salesfroce credentila');
-//     expect(response.statusCode).toBe(200);
+  it('should return  200 ', async () => {
+    const response = await fastify.inject({
+      method: 'get',
+      url: '/',
+      headers: {
+        'X-API-KEY': API_KEY,
+      },
+    });
+    console.log(response, 'Response for salesfroce credentila');
+    expect(response.statusCode).toBe(200);
 
-//   });
+  });
 
-//   it('should return  200 for test route', async () => {
-
-
-//     const response = await fastify.inject({
-//       method: 'get',
-//       url: '/test',
-//       headers: {
-//         'X-API-KEY': API_KEY,
-//       },
-//     });
-//     console.log(response, 'Response for salesfroce credentila');
-//     expect(response.statusCode).toBe(200);
-
-//   });
-
-//   it('should return  403  for  wrong API Key ', async () => {
-//     const response = await fastify.inject({
-//       method: 'get',
-//       url: '/test',
-//       headers: {
-//         'X-API-KEY': 'wrong api key',
-//       },
-//     });
-//     console.log(response, 'Response for salesfroce credentila');
-//     expect(response.statusCode).toBe(403);
-
-//   });
-
-// })
+  it('should return  200 for test route', async () => {
 
 
+    const response = await fastify.inject({
+      method: 'get',
+      url: '/test',
+      headers: {
+        'X-API-KEY': API_KEY,
+      },
+    });
+    console.log(response, 'Response for salesfroce credentila');
+    expect(response.statusCode).toBe(200);
+
+  });
+
+  it('should return  403  for  wrong API Key ', async () => {
+    const response = await fastify.inject({
+      method: 'get',
+      url: '/test',
+      headers: {
+        'X-API-KEY': 'wrong api key',
+      },
+    });
+    console.log(response, 'Response for salesfroce credentila');
+    expect(response.statusCode).toBe(403);
+
+  });
+
+})
 
 
 
 
 
-// describe('POST /api/v1/salesforce/store-credentials', () => {
-//   let fastify: any;
-
-//   beforeAll(async () => {
-//     fastify = await initializeApp(); // Initialize the app instance
-//   });;
 
 
-//   it('should validate the Salesforce credentials and return 200 with correct API key', async () => {
-//     const requestBody = {
-//       clientId: SF_CLIENT_ID,
-//       orgId: SF_ORG_ID,
-//     };
+describe('POST /api/v1/salesforce/store-credentials', () => {
+  let fastify: any;
 
-//     const response = await fastify.inject({
-//       method: 'POST',
-//       url: '/api/v1/salesforce/store-credentials',
-//       payload: requestBody,
-//       headers: {
-//         'X-API-KEY': API_KEY,
-//       },
-//     });
-//     console.log(response, 'Response for salesfroce credentila');
-//     expect(response.statusCode).toBe(200);
-
-//   });
-
-//   it('should validate the Salesforce credentials and return 403 For wrong API KEY', async () => {
-//     const requestBody = {
-//       clientId: SF_CLIENT_ID,
-//       orgId: SF_ORG_ID,
-//     };
-
-//     const response = await fastify.inject({
-//       method: 'POST',
-//       url: '/api/v1/salesforce/store-credentials',
-//       payload: requestBody,
-//       headers: {
-//         'X-API-KEY': 'wrong api key',
-//       },
-//     });
-//     console.log(response, 'Response for salesfroce credentila');
-//     expect(response.statusCode).toBe(403);
-
-//   });
+  beforeAll(async () => {
+    fastify = await initializeApp(); // Initialize the app instance
+  });;
 
 
+  it('should validate the Salesforce credentials and return 200 with correct API key', async () => {
+    const requestBody = {
+      clientId: SF_CLIENT_ID,
+      orgId: SF_ORG_ID,
+    };
 
-//   it('should return 400 for invalid request body', async () => {
-//     const invalidRequestBody = {
-//       clientId: 'INVALID_CLIENT_ID',
-//       orgId: 'INVALID_ORG_ID',
-//     };
+    const response = await fastify.inject({
+      method: 'POST',
+      url: '/api/v1/salesforce/store-credentials',
+      payload: requestBody,
+      headers: {
+        'X-API-KEY': API_KEY,
+      },
+    });
+    console.log(response, 'Response for salesfroce credentila');
+    expect(response.statusCode).toBe(200);
 
-//     const response = await fastify.inject({
-//       method: 'POST',
-//       url: '/api/v1/salesforce/store-credentials',
-//       payload: invalidRequestBody,
-//       headers: {
-//         'X-API-KEY': API_KEY,
-//       },
-//     });
+  });
 
-//     expect(response.statusCode).toBe(400);
-//   });
+  it('should validate the Salesforce credentials and return 403 For wrong API KEY', async () => {
+    const requestBody = {
+      clientId: SF_CLIENT_ID,
+      orgId: SF_ORG_ID,
+    };
 
-//   it('should return 401 for Not Giving API KEY', async () => {
-//     const invalidRequestBody = {
-//       clientId: 'INVALID_CLIENT_ID',
-//       orgId: 'INVALID_ORG_ID',
-//     };
+    const response = await fastify.inject({
+      method: 'POST',
+      url: '/api/v1/salesforce/store-credentials',
+      payload: requestBody,
+      headers: {
+        'X-API-KEY': 'wrong api key',
+      },
+    });
+    console.log(response, 'Response for salesfroce credentila');
+    expect(response.statusCode).toBe(403);
 
-//     const response = await fastify.inject({
-//       method: 'POST',
-//       url: '/api/v1/salesforce/store-credentials',
-//       payload: invalidRequestBody,
+  });
 
-//     });
 
-//     expect(response.statusCode).toBe(401);
-//   });
 
-//   it('should return 500 for invalid request body', async () => {
-//     const invalidPayload = '{ clientId: INVALID_CLIENT_ID, orgId: INVALID_ORG_ID ';
-//     const response = await fastify.inject({
-//       method: 'POST',
-//       url: '/api/v1/salesforce/store-credentials',
-//       payload: invalidPayload,
-//       headers: {
+  it('should return 400 for invalid request body', async () => {
+    const invalidRequestBody = {
+      clientId: 'INVALID_CLIENT_ID',
+      orgId: 'INVALID_ORG_ID',
+    };
 
-//         'X-API-KEY': API_KEY,
-//       },
-//     });
+    const response = await fastify.inject({
+      method: 'POST',
+      url: '/api/v1/salesforce/store-credentials',
+      payload: invalidRequestBody,
+      headers: {
+        'X-API-KEY': API_KEY,
+      },
+    });
 
-//     expect(response.statusCode).toBe(415);
-//   });
+    expect(response.statusCode).toBe(400);
+  });
 
-// })
+  it('should return 401 for Not Giving API KEY', async () => {
+    const invalidRequestBody = {
+      clientId: 'INVALID_CLIENT_ID',
+      orgId: 'INVALID_ORG_ID',
+    };
+
+    const response = await fastify.inject({
+      method: 'POST',
+      url: '/api/v1/salesforce/store-credentials',
+      payload: invalidRequestBody,
+
+    });
+
+    expect(response.statusCode).toBe(401);
+  });
+
+  it('should return 500 for invalid request body', async () => {
+    const invalidPayload = '{ clientId: INVALID_CLIENT_ID, orgId: INVALID_ORG_ID ';
+    const response = await fastify.inject({
+      method: 'POST',
+      url: '/api/v1/salesforce/store-credentials',
+      payload: invalidPayload,
+      headers: {
+
+        'X-API-KEY': API_KEY,
+      },
+    });
+
+    expect(response.statusCode).toBe(415);
+  });
+
+})
 
 
 describe('POST /api/v1/salesforce/process-document', () => {
@@ -308,6 +308,51 @@ let originalServiceAccount
     console.log(response, 'Response for process document');
     expect(response.statusCode).toBe(400);
     process.env.SERVICE_ACCOUNT = originalServiceAccount
+  });
+
+  it('Create new secret Manager Id with new Org id', async () => {
+    const requestBody = {
+      "orgId": "00DFF00000BoiXu",
+      "userName": "cddev@org.com",
+      "recordId": "001WU00000Tv8bLYAR",
+      "fileName": "Account",
+      "contentVersionId": "068WU000005J3aPYAS",
+      "fieldData": [
+        {
+          "Account": {
+            "Name": "Financial Insights LLC",
+            "BillingStreet": "",
+            "BillingCity": "Boston",
+            "BillingCountry": "USA",
+            "Phone": "+1 555-7890"
+          },
+          "ProductDetails": {
+            "Name": "Financial Analysis Tool Investment Strategy Simulator",
+            "Product2": {
+              "Name": "Investment Strategy Simulator"
+            },
+            "Quantity": 6,
+            "UnitPrice": 299,
+            "TotalPrice": 1794,
+            "Discount__c": 10,
+            "DiscountAmtQI__c": 179.4,
+            "DiscountedFinalQIPrice__c": 1614.6
+          }
+        }
+      ]
+    }
+    console.log(requestBody, 'requestBody');
+    const response = await fastify.inject({
+      method: 'POST',
+      url: '/api/v1/salesforce/process-document',
+      payload: requestBody,
+      headers: {
+        'X-API-KEY': 'AIzaSyArxb3xZ5lTVpGrF6YbMsCrS9e8iPGLldY',
+      },
+    });
+    console.log(response.statusCode, 'Response for process document');
+    expect(response.statusCode).toBe(200);
+
   });
 
 })
