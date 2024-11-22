24836351011a80daa369fc69464abc3ad6d09b99
pravin-on-cloud <pravinraja@clouddesk.ae>
2024-11-22 19:01:01 +0530

    updated yaml

--- src/tests/initialtest.test.ts
+++ src/tests/initialtest.test.ts
@@ -5,6 +5,8 @@ import { cleanupCredentials } from '../utils/clearCrendtials.js';
 describe('Initial test', () => {
     let fastify: any;
 console.log('test')
+console.log('test')
+console.log('test')
     beforeAll(async () => {
         fastify = await initializeApp(); // Initialize the app instance
 
