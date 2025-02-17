2f0f7a6c46e35892b9bae8edcd393de3ad4dad44
pravin-on-cloud <pravinraja@clouddesk.ae>
2025-02-17 15:59:27 +0530

    builds

---
+++ build/index.js
@@ -0,0 +1,101 @@
+import { fastify } from "fastify";
+import { docGenRouter } from "./router/router.js";
+import { BASE_URL, DOCGEN_API_KEY, PORT } from "./config/config.js";
+import cors from "@fastify/cors";
+import swagger from "@fastify/swagger";
+import swaggerUi from "@fastify/swagger-ui";
+const Fastify = fastify({ logger: false });
+console.log(DOCGEN_API_KEY, "API key from secret manager DOCGEN_API_KEY");
+function setupSwagger(fastifyInstance) {
+    console.log('inside setupSwagger');
+    const SWAGGER_URL = BASE_URL;
+    fastifyInstance.register(swagger, {
+        openapi: {
+            info: {
+                title: "Doc Gen Api Documentation",
+                description: "API documentation with API key authentication",
+                version: "1.0.0",
+            },
+            servers: [{ url: SWAGGER_URL }],
+            components: {
+                securitySchemes: {
+                    ApiKeyAuth: {
+                        type: "apiKey",
+                        name: "x-api-key",
+                        in: "header",
+                        description: "API key required for protected endpoints.",
+                    },
+                },
+            },
+            paths: {},
+        },
+    });
+    fastifyInstance.register(swaggerUi, {
+        routePrefix: "/docs",
+        staticCSP: true,
+        uiConfig: {
+            docExpansion: "full",
+            deepLinking: false,
+            tagsSorter: "alpha",
+            operationsSorter: "alpha",
+        },
+    });
+}
+function setupCors(fastifyInstance) {
+    fastifyInstance.register(cors, {
+        origin: true, // Adjust for production
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization", "Accept"],
+        credentials: true,
+        maxAge: 86400,
+        exposedHeaders: ["set-cookie"],
+    });
+}
+async function apiKeyValidationHook(request, reply) {
+    try {
+        const swaggerRoutes = ["/docs", "/docs/*"];
+        if (swaggerRoutes.some((route) => request.url?.startsWith(route)) ||
+            request.url === "/") {
+            return; // Allow requests to Swagger documentation without API key
+        }
+        console.log('inside hhook');
+        console.log(request.headers, 'request.headers');
+        const headerApiKey = request.headers["x-api-key"];
+        console.log(headerApiKey, "headerApiKey");
+        console.log(request.body, 'global hook check');
+        if (!headerApiKey) {
+            return reply.status(401).send({
+                error: 'API key is missing or invalid. Please include a valid API key in the "x-api-key" header to access this endpoint.',
+            });
+        }
+        if (headerApiKey !== DOCGEN_API_KEY) {
+            return reply.status(403).send({
+                error: "Access denied. The provided API key is incorrect. Ensure you are using the correct API key to access this route.",
+            });
+        }
+    }
+    catch (error) {
+        console.log(error, `error in global hook`);
+    }
+}
+Fastify.addHook("onRequest", apiKeyValidationHook);
+setupSwagger(Fastify);
+setupCors(Fastify);
+Fastify.register(docGenRouter);
+const start = async () => {
+    try {
+        await Fastify.listen({ port: PORT, host: "0.0.0.0" });
+        console.log(`Server is running on port ${PORT}`);
+    }
+    catch (err) {
+        console.error("Error starting server:", err.message);
+    }
+};
+start();
+async function initializeApp() {
+    await Fastify.ready(); // Ensure Fastify is initialized
+    return Fastify;
+}
+export const app = Fastify;
+export { initializeApp, DOCGEN_API_KEY };
+//# sourceMappingURL=index.js.map
---
+++ build/router/router.js
@@ -0,0 +1,32 @@
+import { validateRequestBody } from "../ajv/validation.js";
+import { documentController } from "../controller/document.controller.js";
+import { sfCredentialController } from "../controller/sfcredential.controller.js";
+import { sfOrgIdClientIdValidation } from "../schema/validateSalesforceData.js";
+import { sfValidateTemplateData } from "../schema/sfuserdatavalidation.js";
+import { documentControllerTask } from "../controller/documenttask.controller.js";
+export const docGenRouter = (fastify, options, done) => {
+    // Root route
+    fastify.get("/", (request, reply) => {
+        console.log("Root route accessed");
+        reply.send("Successfully Worked DocGen Testing");
+    });
+    // Salesforce ID validation route
+    fastify.post("/api/v1/salesforce/store-credentials", {
+        // schema: [sfOrgClientIdSwagger],
+        preHandler: [validateRequestBody(sfOrgIdClientIdValidation)],
+    }, sfCredentialController.validateAndStoreSalesforceCredentials);
+    fastify.post("/api/v1/salesforce/process-document", {
+        // schema: [processDocumentSwagger],
+        preHandler: [validateRequestBody(sfValidateTemplateData)],
+    }, 
+    //create task entry
+    documentController.processDocument);
+    fastify.post("/api/v1/task/salesforce/process-document", {
+        // schema: [processDocumentSwagger],
+        preHandler: [validateRequestBody(sfValidateTemplateData)],
+    }, 
+    //create task entry
+    documentControllerTask.processDocumentTask);
+    done();
+};
+//# sourceMappingURL=router.js.map
